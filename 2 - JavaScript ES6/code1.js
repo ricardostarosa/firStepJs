9e9f7dd267141e4b4b45bc0b2e18fd6748d45c32
ricardo <rosastaricardo@gmail.com>
2021-10-04 13:33:59 -0300

    unary element

--- 2 - JavaScript ES6/code1.js
+++ 2 - JavaScript ES6/code1.js
@@ -1,4 +1,9 @@
 
-let texto = 'text';
 
-console.log(texto.length);
+// Unary elements são operações representada por uma palavra "typeof" cujo retorno fornece o tipo do valor.
+
+let elementos = ['text', 23 , null];
+
+console.log(` Elemento do tipo: ${typeof elementos[0]}, elemento : ${typeof elementos[1]}, elemento:  ${typeof elementos[2]}`);
+
+
