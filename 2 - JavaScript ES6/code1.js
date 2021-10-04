1b6f6be091388d1d96efc751039c0f8365ebfd67
ricardo <rosastaricardo@gmail.com>
2021-10-04 13:50:06 -0300

    binary operator

--- 2 - JavaScript ES6/code1.js
+++ 2 - JavaScript ES6/code1.js
@@ -6,4 +6,6 @@ let elementos = ['text', 23 , null];
 
 console.log(` Elemento do tipo: ${typeof elementos[0]}, elemento : ${typeof elementos[1]}, elemento:  ${typeof elementos[2]}`);
 
+// binary elements são operadores que retornam valores lógicos do tipo: verdadeiro ou falso.
 
+console.log ( 2+3 > 7 && 10 > 4);
