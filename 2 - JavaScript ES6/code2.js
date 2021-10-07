b57fdac8bf1c08bda8366cc7bea5baa76861eff5
ricardo <rosastaricardo@gmail.com>
2021-10-07 19:26:50 -0300

    fizzBuzz

--- 2 - JavaScript ES6/code2.js
+++ 2 - JavaScript ES6/code2.js
@@ -8,3 +8,13 @@ for (let x = '#'; x.length <= 7; x += '#'){
 
 
 
+for(let number = 1; number <= 100 ; number++){
+
+        
+    if(number % 3 == 0 && number % 5 == 0) console.log('FizzBuzz');
+    else if(number % 3 == 0) console.log('Fizz');
+    else if(number % 5 == 0) console.log('Buzz');
+    else console.log(number);
+}
+
+
