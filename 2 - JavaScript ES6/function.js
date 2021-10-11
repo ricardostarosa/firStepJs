0de92eb871ef262f46bd9bf2032e71a318bccf8c
ricardo <rosastaricardo@gmail.com>
2021-10-11 01:41:57 -0300

    closure

--- 2 - JavaScript ES6/function.js
+++ 2 - JavaScript ES6/function.js
@@ -26,4 +26,18 @@ const calcularPotencia = function(base,expoente){
 
 console.log(calcularPotencia(5,3));
 
+// call stack - the function return to the caller after the execution of the function
 
+// closure - The ability to treat functions as values, combined with the fact that
+// local bindings are re-created every time a function is called
+
+function multiplier(factor) {
+    return number => number * factor;
+    }
+    let twice = multiplier(2);
+    console.log(twice(5));
+
+let teste = (x) => { return y => (y * x) + 5 };
+
+let segunda = teste(2);
+console.log(segunda(3));
